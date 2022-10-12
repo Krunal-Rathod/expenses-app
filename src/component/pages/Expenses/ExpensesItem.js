c8c4c80ab2d94dff2a45a0fe7e16378192fc3c81
Krunal Rathod <krunal.rathod@ezeetechnosys.com>
2022-10-12 15:33:47 +0530

    New Update

--- src/component/pages/Expenses/ExpensesItem.js
+++ src/component/pages/Expenses/ExpensesItem.js
@@ -4,16 +4,19 @@ import Expense from './Expense';
 const ExpensesItem = () => {
     const expenses = [
         {
+            id: 1,
             title: 'Bike Insurance',
             date: new Date('2022-10-12'),
             price: 78.2
         },
         {
+            id: 2,
             title: 'Personal Cost',
             date: new Date('2022-10-22'),
             price: 18.17
         },
         {
+            id: 3,
             title: 'Travel Expense',
             date: new Date('2022-10-18'),
             price: 10
@@ -25,11 +28,12 @@ const ExpensesItem = () => {
                 <div className='container'>
                     {
                         expenses.map((props) => (
-                            <Expense expenseName={props.name} expensePrice={props.price} expenseDate={props.date} />
+                            <Expense expenseName={props.title} expensePrice={props.price} expenseDate={props.date} />
                         ))
                     }
                 </div>
             </section>
+            <section></section>
         </>
     )
 }
--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -57,8 +57,34 @@ const Home = () => {
                         <SwiperSlide>Slide 8</SwiperSlide>
                         <SwiperSlide>Slide 9</SwiperSlide>
                     </Swiper>
+
+                    <div className="contact__form mt-5 bg-white">
+                        <h2 className="mb-3">Let's Connect</h2>
+                        <form id="contactform" method="post">
+                            <div className="form-group">
+                                <label>Company Name</label>
+                                <input type="text" name="company" className="form-control" placeholder="Your Company Name" />
+                            </div>
+                            <div className="form-group">
+                                <label>Name</label>
+                                <input type="text" name="name" className="form-control" required="" placeholder="Your Full Name" />
+                            </div>
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input type="email" name="email" className="form-control" required="" placeholder="Enter Your E-Mail Address" />
+                            </div>
+                            <div className="form-group">
+                                <label>Phone</label>
+                                <input type="text" name="mobile" className="form-control" required="" placeholder="Enter Your Contact Number" />
+                            </div>
+                            <div className="form-group mt-5 mb-0">
+                                <button type="submit" className="btn btn-primary btn-block">Send</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </section>
+
         </>
     )
 }
