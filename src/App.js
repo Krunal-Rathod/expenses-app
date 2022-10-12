0d90e6c904b4976c6ac89f37f8856d5b4756cbab
Krunal Rathod <krunal.rathod@ezeetechnosys.com>
2022-10-12 14:51:40 +0530

    Expense App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './component/includes/Header'
 import Footer from './component/includes/Footer';
 import Home from './component/pages/Home';
 import About from './component/pages/About';
-import ExpensesItem from './component/includes/Expenses/ExpensesItem';
+import ExpensesItem from './component/pages/Expenses/ExpensesItem';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
